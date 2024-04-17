33f9a7606137b06fb8eb83cbd4fae3960047416f
busra <kocarslanbusrab@gmail.com>
2024-04-17 17:15:22 +0300

    image change

--- src/pages/about/More.jsx
+++ src/pages/about/More.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./About.scss"
-import ankkedi6 from "../../assets/img/ankkedi6.jpg"
+import ankkedi4 from "../../assets/img/ankkedi4.webp";
 
 
 const More = () => {
@@ -8,7 +8,7 @@ const More = () => {
     <div>
         <p>In 1962, many American military personnel saw Angora Cats, the product of a 45-year breeding program, at the Ankara Zoo, and the military personnel who took a few pairs back to their country helped revive interest in this breed. These slim cats with silky, medium-length fur are loved and continue to be bred with interest in all countries that know and enjoy the beauty of cats.</p>
         <img style={{width:"250px",
-            borderRadius: "10px"}} src={ankkedi6} alt="" />
+            borderRadius: "10px"}} src={ankkedi4} alt="" />
     </div>
   )
 }
